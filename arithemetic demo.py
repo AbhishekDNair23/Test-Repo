9c80a9b31132428d13162631992c8b98faf77d45
AbhishekDNair23 <abhishekdnair2028@cs.sjcetpalai.ac.in>
2024-10-18 15:58:27 +0530

    Add files via upload

---
+++ arithemetic demo.py
@@ -0,0 +1,7 @@
+number1 = int(input("Enter the first number:"))
+number2 = int(input("Enter the second number:"))
+print("SUM=",number1+number2,"Divison=",number1/number2)
+print("Is number1 greater than number2?:", number1>number2)
+print("number1 and number 2 equal?",number1==number2)
+print("Logical AND ",number1>0 and number2>0)
+print("Logical OR ",number1>0 or number2>10)
