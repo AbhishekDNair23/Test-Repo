aadcc0775f3a27b10b66a4fb81326f53321110d7
AbhishekDNair23 <abhishekdnair2028@cs.sjcetpalai.ac.in>
2024-10-19 14:46:47 +0530

    Add files via upload

---
+++ basiarithemetic.demo.py
@@ -0,0 +1,16 @@
+number_1 = int(input("enter number_1"))
+number_2 = int(input("enter number_2"))
+number_3 = int(input("enter number_3"))
+sum = number_1+number_2
+print("the sum of number_1 and number2 is:",sum)
+difference = number_2-number_1
+print("The difference when number_2 is subtracted from number_1 is:",difference)
+multiple = number_1*number_2
+print("The product of number_1 and number_2 is:",multiple)
+division = number_1/number_2
+print("The quotient when number_1 is divided by number_2 is:",division)
+modulus = number_1%number_2
+print("The remainder when number_1 is divided by number_2 is:",modulus)
+result = (number_1+number_2)*number_3/2
+print("The result of (num1 + num2) * num3 / 2 is:",result)
+
