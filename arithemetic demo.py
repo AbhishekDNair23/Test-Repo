967073edeb555b0a2592ee63d7aab69121cabf5a
AbhishekDNair23 <abhishekdnair2028@cs.sjcetpalai.ac.in>
2024-10-24 20:38:02 +0530

    Update arithemetic demo.py

--- arithemetic demo.py
+++ arithemetic demo.py
@@ -3,5 +3,5 @@ b=5
 print("Sum",a+b,"Divison",a/b)
 print("Is a greater than b?:", a>b)
 print("a and b equal?:",a==b)
-print("Logical AND ",a>0 and b>0)
-print("Logical OR ",a>0 or b>10)
+print("Logical AND :",a>0 and b>0)
+print("Logical OR :",a>0 or b>10)
